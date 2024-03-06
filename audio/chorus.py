daa68f5458e92632dae7ef7f91af3bd4bf740750
Brian Connolly <brian.j.connolly.aero@gmail.com>
2024-03-05 21:46:02 -0700

    Updated item routes (untested)

--- audio/chorus.py
+++ audio/chorus.py
@@ -61,7 +61,7 @@ def apply_chorus(y, sr, depth=0.005, rate=.1):
 
 if __name__=='__main__':
     # Example usage
-    input_audio = 'cheevo_ai.mp3'
-    output_audio = 'cheevo_ai_out.wav'
-    n_steps = 6  # Shifts pitch up by 4 half-steps. Use negative values to shift down.
+    input_audio = 'cheevo_ai2.mp3'
+    output_audio = 'cheevo_ai2_out.wav'
+    n_steps = 4  # Shifts pitch up by 4 half-steps. Use negative values to shift down.
     pitch_shift_audio(input_audio, output_audio, n_steps)
